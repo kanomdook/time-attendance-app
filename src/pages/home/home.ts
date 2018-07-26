cbf9cab5580222b02aec1d85e20228e878b185a1
kanomdook <kanung.2535@gmail.com>
2018-07-26 16:24:16 +0700

    [add] finger print

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, FabContainer } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('fab')fab : FabContainer;
+  time: string = '00:00:00';
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.getTime();
+    this.fab.toggleList();
+  }
+
+  getTime() {
+    setInterval(() => {
+      let date = new Date();
+      let H: any = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+      let M: any = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+      let S: any = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+      this.time = H + ':' + M + ':' + S;
+    }, 1000);
   }
 
 }
